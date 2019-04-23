bdcecb9870486b3b00a3979610630fae13f6bd6a
Batmancarlo <43032221+Batmancarlo@users.noreply.github.com>
2019-04-23 18:28:24 +0200

    moved navbar to Appvue and made logo a home button

--- Photobookvueapplication/Photobookvueapplication/src/App.vue
+++ Photobookvueapplication/Photobookvueapplication/src/App.vue
@@ -1,8 +1,22 @@
 <template>
+    
     <div id="app">
-        
+        <header class="topnav">
+            <nav>
+                <router-link tag="img" src="images/titleonlyp.png" class="alignleft" to="/"></router-link>
+                
+                <a class="alignleft" href="hostsignuppage.html"> Signup as Event Host</a>
+                <a class="alignright"> Privacy</a>
+                <a class="alignright"> About Photobook</a>
+            </nav>
+
+
+        </header>
+
+        <br /> <br />
+
         <router-view></router-view>
-        <!--<Home msg="Hello world!" />-->
+
     </div>
 </template>
 
---
+++ Photobookvueapplication/Photobookvueapplication/src/components/navbar.vue
@@ -0,0 +1,9 @@
+﻿<template>
+    
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
