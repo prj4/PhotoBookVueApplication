3c9b59a0ee9a7055f49362f3cfd98c126b207cbd
Batmancarlo <43032221+Batmancarlo@users.noreply.github.com>
2019-05-02 17:09:55 +0200

    Host Sign up fungere, mangler lidt routing og cookie håndtering

--- Photobookvueapplication/Photobookvueapplication/src/components/HostSignUp.vue
+++ Photobookvueapplication/Photobookvueapplication/src/components/HostSignUp.vue
@@ -1,7 +1,7 @@
 ﻿<template>
     <div id="Wrapper">
         <img src="images/photobooktitle.png" width="90%" height="90%" />
-        <form id="signup" method="post" @submit="postHostSignUp" >
+        <form >
             <input class="textbox" type="name" v-model="HostName" placeholder="Name" required /> <br /> <br />
             <input class="textbox" type="email" v-model="HostEmail" placeholder="Email" required /> <br /> <br />
             <input class="textbox" type="password" v-model="HostPassword" placeholder="Password" required pattern="[\w]{6,}"/> <br /> <br />
@@ -9,7 +9,7 @@
 
         </form>
         <router-link to="/hostlogin">
-            <button class="button" type="button" width="100px" Height="50px"> Sign up as Host</button>
+            <button class="button" type="button" v-on:click="postHostSignUp" width="100px" Height="50px"> Sign up as Host</button>
         </router-link>
 
     </div>
@@ -23,16 +23,15 @@
                 var url = 'https://photobookwebapi1.azurewebsites.net/api/Account/Host'
 
                 var data = {
-                    userName: this.HostName,
-                    email: this.HostEmail,
-                    passWord: this.HostPassword,
-                    confirmPassword: this.ConfirmPassword,
+                    Name: this.HostName,
+                    Email: this.HostEmail,
+                    Password: this.HostPassword,
                 };
 
-
-
+                
                 fetch(url, {
                     method: 'POST',
+                    credential: 'include',
                     headers: new Headers({
                         'Content-Type': 'application/json', 'Accept': 'application/json'
                     }),
@@ -41,6 +40,10 @@
 
                 }).then(response => response.json())
                     .then(data => this.message = data)
+                    .then(function (data){
+
+                    })
+                
             }
         },
         data() {
